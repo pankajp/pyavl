8ca1db9477f28eaaa13b60f2248ab1541e3693c3
Pankaj Pandey <pankaj86@gmail.com>
2009-06-29 12:26:42 +0000

    geometry viewer works for homogeneous sections in a surface
    git-svn-id: file:///home/pankaj/opt/idearesearchrepo@38 c2e9ef4a-c839-440f-974a-6fdc79ea441c

--- pyavl/ui/geometry_viewer.py
+++ pyavl/ui/geometry_viewer.py
@@ -64,12 +64,16 @@ class SectionViewer(HasTraits):
 class SurfaceViewer(HasTraits):
     # sorted by span y coordinate
     surface = Instance(Surface)
+    # section chords : leading edge,trailing edge to be set by sectiondata
+    section_chords = Array(dtype=numpy.float, shape=(None,2,3))
     # min 2 points in a section
     # array : section : point : xyz
     sectiondata = Property(Array(dtype=numpy.float, shape=(None,(2, None), 3)), depends_on='surface')
+    surfacedata = Property(Array(dtype=numpy.float, shape=(None,None,3)), depends_on='sectiondata')
     @cached_property
     def _get_sectiondata(self):
         ret = []
+        chords = []
         for section in self.surface.sections:
             # FIXME: Assumption: section data is from x=0 to x=1
             dataxz = section.data.get_data_points()
@@ -82,31 +86,53 @@ class SurfaceViewer(HasTraits):
             # rotate by the angle = section+surface angle
             angle = section.angle + self.surface.angle
             c, s = numpy.cos(angle * numpy.pi / 180), numpy.sin(angle * numpy.pi / 180)
-            data[:, 0] *= c
-            data[:, 0] += data[:, 2] * s
-            data[:, 2] *= c
-            data[:, 2] -= data[:, 0] * s
+            data[:,0], data[:,2] =  data[:,0]*c + data[:,2]*s, data[:,2]*c - data[:,0]*s
             # translate to the leading edge
             data += section.leading_edge
             ret.append(data)
+            # get trailing edge
+            te = numpy.array([section.chord*c,
+                              0,
+                              -section.chord*s])
+            te += section.leading_edge
+            #assert data[1,2] == te[2]
+            chords.append(numpy.array([section.leading_edge, te]))
             if numpy.isfinite(self.surface.yduplicate):
                 #print 'ydup'
                 data2 = numpy.empty(shape=data.shape)
                 data2[:,0] = data[:,0]
                 data2[:,2] = data[:,2]
                 data2[:,1] = self.surface.yduplicate - data[:,1]
+                le2 = numpy.array([section.leading_edge[0], self.surface.yduplicate-section.leading_edge[1], section.leading_edge[2]])
+                te2 = numpy.array([te[0], self.surface.yduplicate-te[1], te[2]])
                 #print data[:,1], data2[:,1]
                 ret.append(data2)
+                chords.append(numpy.array([le2, te2]))
+        #print ret
         out = numpy.array(ret)
         # sort by y position of the section
-        out = out[numpy.argsort(out[:,0,1])]
+        arg_sort = numpy.argsort(out[:,0,1])
+        out = out[arg_sort]
+        chords = numpy.array(chords)[arg_sort]
         # surface transforms
         #print('Scale : %s' %str(self.surface.scale))
         #assert all(self.surface.scale > 0)
         out *= self.surface.scale
         out += self.surface.translate
+        chords *= self.surface.scale
+        chords += self.surface.translate
         # angle rotation done for each section separately
+        self.section_chords = chords
+        assert len(self.section_chords) == len(out)
         return out
+    
+    @cached_property
+    def _get_surfacedata(self, num_sectionpts=6):
+        ret = numpy.empty((self.sectiondata.shape[0], num_sectionpts, 3))
+        for i,section in enumerate(self.section_chords):
+            r = linspace(0,1,num_sectionpts).reshape(num_sectionpts,1)
+            ret[i,:,:] = section[0,:] + numpy.dot(r,(section[1,:]-section[0,:]).reshape(1,3))
+        return ret
     
     traits_view = View()
 
@@ -176,8 +202,10 @@ class GeometryViewer(HasTraits):
         self.scene.mlab.clf()
         for surface in self.surfaces:
             section_pts = surface.sectiondata
+            print section_pts
             for i in xrange(0, section_pts.shape[0]):
                 self.plots.append(self.scene.mlab.plot3d(section_pts[i, :, 0], section_pts[i, :, 1], section_pts[i, :, 2]))
+            self.plots.append(self.scene.mlab.mesh(surface.surfacedata[:,:,0], surface.surfacedata[:,:,1], surface.surfacedata[:,:,2]))
         print 'numplots = ', len(self.plots)
     
     #@on_trait_change('geometry')
@@ -201,12 +229,12 @@ class GeometryViewer(HasTraits):
 
 if __name__ == '__main__':
     from pyavl.case import Case
-    file = open('/opt/idearesearch/avl/runs/allegro.avl')
+    file = open('/opt/idearesearch/avl/runs/h.avl.mod')
     case = Case.case_from_input_file(file)
     g = GeometryViewer(geometry=case.geometry)
-    print g.surfaces[2].sectiondata
+    #print g.surfaces[2].sectiondata
     g.configure_traits()
-    sections = case.geometry.surfaces[2].sections
+    #sections = case.geometry.surfaces[2].sections
     section = None
     for s in sections:
         if s.type == 'airfoil data file':
